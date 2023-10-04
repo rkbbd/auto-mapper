57629d2ff6a8b998da6414da005c4738fadc9c52
MD RAKIB HASAN <rakib424@gmail.com>
2023-10-04 12:32:20 +0600

    initial

---
+++ AutoMapperApp/AutoMapperApp/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace AutoMapperApp
+{
+    public class Mapper<TSource, TDestination>
+    {
+        public TDestination Map(TSource source)
+        {
+            var destination = Activator.CreateInstance<TDestination> ();
+            foreach (var sProperty in typeof(TSource).GetProperties())
+            {
+                var dProperty = typeof(TDestination).GetProperty(sProperty.Name);
+                if (dProperty != null)
+                {
+                    dProperty.SetValue (destination, sProperty.GetValue(source));
+                }
+            }
+            return destination;
+        }
+        public List<TDestination> Map(List<TSource> sources)
+        {
+           return sources.Select(source => Map(source)).ToList();
+        }
+
+    }
+}
